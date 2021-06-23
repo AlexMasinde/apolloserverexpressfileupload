23d3dbb9bdcb9b871e29f1ffd0eee3ac4ffcc431
AlexMasinde <56753745+AlexMasinde@users.noreply.github.com>
2021-06-23 11:22:57 +0300

    Delete bin directory

--- bin/upload-avatar.sh
+++
@@ -1,4 +0,0 @@
-curl $1 \
-  -F operations='{ "query": "mutation ($file: Upload!) { uploadFile(file: $file) { uri filename mimetype encoding } }", "variables": { "file": null } }' \
-  -F map='{ "0": ["variables.file"] }' \
-  -F 0=@$2
